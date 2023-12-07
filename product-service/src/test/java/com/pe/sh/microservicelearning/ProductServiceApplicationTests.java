3fad03f4e63b83d18ab49e384723c4a7489fa6b0
Samuel <shmenzala@gmail.com>
2023-12-07 14:08:55 -0500

    Add modifications (testContainters)

--- product-service/src/test/java/com/pe/sh/microservicelearning/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/pe/sh/microservicelearning/ProductServiceApplicationTests.java
@@ -1,13 +1,30 @@
 package com.pe.sh.microservicelearning;
 
+import com.pe.sh.microservicelearning.repository.ProductosRepository;
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+//@SpringBootTest
+//@Testcontainers
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//    @Container
+//    @ServiceConnection
+//    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:6.0.4");
+//    
+//    @Autowired
+//    ProductosRepository productRepository;
+//    
+    @Test
+    void contextLoads() {
+//        AssertionsForClassTypes.assertThat(mongoDBContainer.isCreated()).isTrue();
+//        AssertionsForClassTypes.assertThat(mongoDBContainer.isRunning()).isTrue();
+    }
 
 }
